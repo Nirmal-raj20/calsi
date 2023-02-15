a205eb511bf4b362cf7c9dd309f2760b81b4258e
Nirmal-raj20 <125450072+Nirmal-raj20@users.noreply.github.com>
2023-02-15 16:03:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function sum(){
+    var n1,n2,s;
+    n1=parseInt(document.getElementById("fnum").value)
+    n2=parseInt(document.getElementById("snum").value)
+    s=n1+n2
+    document.getElementById("ans").value=s
+}
